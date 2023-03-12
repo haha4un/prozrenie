00da98ada5dada32d208bc2e426d4d12fb88732d
haha4un <savelijstulev3872@gmail.com>
2023-03-12 12:57:38 +0500

    3.2

--- app/src/main/java/com/example/prozrenie/create_note.kt
+++ app/src/main/java/com/example/prozrenie/create_note.kt
@@ -1,11 +1,11 @@
 package com.example.prozrenie
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -21,30 +21,27 @@ class create_note : AppCompatActivity() {
 
         var s = findViewById<Button>(R.id.save_newNote)
         var n = findViewById<EditText>(R.id.Newnote)
-
         var i = 0
+
+        val database = Firebase.database
+        var ref = database.getReference("main/${info.toString()}/lessons")
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                i = dataSnapshot.childrenCount.toInt()
+            }
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
+
         s.setOnClickListener {
             if (n.text.isBlank()||n.text.isEmpty()) {
                 Toast.makeText(this, "Заметка должна содержать больше символов!", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
             else {
-            val database = Firebase.database
-            val ref = database.getReference("main/${info.toString()}/lessons")
-            ref.addValueEventListener(object: ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-//                    ref.child("main/${info.toString()}/lessons")
-                    for (g in snapshot.children)
-                    {
-                        i = snapshot.children.count().toInt()
-                        ref.child("/$i").setValue(n.text.toString())
-                        return
-                    }
-                }
-                override fun onCancelled(error: DatabaseError) {
-                }
-            })
-                var intent = Intent(this, View_Edit_Card::class.java)
+                ref = database.getReference("main/${info.toString()}/lessons")
+                ref.child("/$i").setValue(n.text.toString())
+                var intent = Intent(this, View_Card::class.java)
                 startActivity(intent)
                 return@setOnClickListener
             }
