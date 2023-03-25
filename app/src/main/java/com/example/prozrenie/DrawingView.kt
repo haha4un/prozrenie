3c0f275ee964e774e6da70f0e13b1ac9c013c6cf
haha4un <savelijstulev3872@gmail.com>
2023-03-25 08:41:32 +0500

    4.3

--- app/src/main/java/com/example/prozrenie/DrawingView.kt
+++ app/src/main/java/com/example/prozrenie/DrawingView.kt
@@ -5,84 +5,112 @@ import android.graphics.*
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
+import com.example.prozrenie.paint
+import com.example.prozrenie.paint.Companion.brushs
+import com.example.prozrenie.paint.Companion.path
 
 class DrawingView(context: Context, attr: AttributeSet): View(context, attr) {
 
-    // Thank you, devlifepl!
-
-    // declaration of variables
-    private var mDrawPath: CustomPath? = null
-    private var mCanvasBitmap: Bitmap? = null
-    private var mDrawPaint: Paint? = null
-    private var mCanvasPaint: Paint? = null
-    private var mBrushSize: Float = 0.0F
-    private var color = Color.GREEN
-    private var canvas: Canvas? = null
-
-    fun edit(c: String, s: Float)
+    var params: ViewGroup.LayoutParams? =null
+    companion object
     {
-        this.color = Color.parseColor(c)
-        this.mBrushSize = s
+        var pathList = ArrayList<Path>()
+        var colorList = ArrayList<Int>()
+        var currentBrush = Color.GREEN
     }
+
+//    // declaration of variables
+//    private var mDrawPath: CustomPath? = null
+//    private var mCanvasBitmap: Bitmap? = null
+//    private var mDrawPaint: Paint? = null
+//    private var mCanvasPaint: Paint? = null
+//    private var mBrushSize: Float = 10.0F
+//    private var color = Color.GREEN
+//    private var canvas: Canvas? = null
+
+
+//    fun edit(c: String, s: Float)
+//    {
+//        this.color = Color.parseColor(c)
+//        this.mBrushSize = s
+//    }
+//    fun DrawingView(Path: Path, Paint: Paint, c: String, s: Float)
+//    {
+//        mDrawPath = CustomPath(Color.parseColor(c),s)
+//        mDrawPaint = Paint
+//    }
+
+//    fun reset()
+//    {
+//        mDrawPath!!.reset()
+//    }
     init{
-        setUpDrawing()
+        brushs.isAntiAlias = true
+        brushs.color = currentBrush
+        brushs.style = Paint.Style.STROKE
+        brushs.strokeJoin = Paint.Join.ROUND
+        brushs.strokeWidth = 10f
+
+        params = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+//        setUpDrawing()
     }
 
-    private fun setUpDrawing(){
-        mDrawPaint = Paint()
-        mDrawPath = CustomPath(color, mBrushSize)
-        mDrawPaint!!.color = color
-        mDrawPaint!!.style = Paint.Style.STROKE
-        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
-        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
-        mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.0F
-
-    }
+//    private fun setUpDrawing(){
+//        mDrawPaint = Paint()
+//        mDrawPath = CustomPath(color, mBrushSize)
+//        mDrawPaint!!.color = color
+//        mDrawPaint!!.style = Paint.Style.STROKE
+//        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+//        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+//        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+//        mBrushSize = 20f
+//    }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 
-        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
-        canvas = Canvas(mCanvasBitmap!!)
+//        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+//        canvas = Canvas(mCanvasBitmap!!)
 
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.drawBitmap(mCanvasBitmap!!, 0.0F, 0.0F, mCanvasPaint)
-        if(!mDrawPath!!.isEmpty) {
-            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
-            mDrawPaint!!.color = mDrawPath!!.color
-            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        for (i in pathList.indices)
+        {
+            brushs.color = colorList[i]
+            canvas.drawPath(pathList[i], brushs)
+            invalidate()
         }
+//        canvas.drawBitmap(mCanvasBitmap!!, 0.0F, 0.0F, mCanvasPaint)
+//        if(!mDrawPath!!.isEmpty) {
+//            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+//            mDrawPaint!!.color = mDrawPath!!.color
+//            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+//        }
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        val touchX = event?.x
-        val touchY = event?.y
+        val x = event?.x
+        val y = event?.y
 
         when(event?.action){
             MotionEvent.ACTION_DOWN -> {
-                mDrawPath!!.color = color
-                mDrawPath!!.brushThickness = mBrushSize
+//                path.color = color
+//                path.brushThickness = mBrushSize
 //                mDrawPath!!.reset()
-                mDrawPath!!.moveTo(touchX!!, touchY!!)
+                path.moveTo(x!!, y!!)
             }
-
             MotionEvent.ACTION_MOVE -> {
-                mDrawPath!!.lineTo(touchX!!, touchY!!)
+//                path.lineTo(touchX!!, touchY!!)
+                path.lineTo(x!!, y!!)
+                pathList.add(path)
+                colorList.add(currentBrush)
             }
-
-            MotionEvent.ACTION_POINTER_UP -> {
-                mDrawPath = CustomPath(color, mBrushSize)
-            }
-
             else -> return false
         }
-
-        invalidate()
-
+        postInvalidate()
         return true
     }
 
--- app/src/main/java/com/example/prozrenie/paint.kt
+++ app/src/main/java/com/example/prozrenie/paint.kt
@@ -1,26 +1,54 @@
 package com.example.prozrenie
 
-import android.annotation.SuppressLint
+import android.graphics.Paint
+import android.graphics.Path
 import android.os.Bundle
-import android.util.AttributeSet
-import android.util.Xml
+import android.provider.MediaStore
+import android.widget.ImageButton
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.drawingapp.DrawingView
-import org.xmlpull.v1.XmlPullParser
+import java.util.*
 
 
 class paint : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId", "ResourceType")
+
+    private var dv: DrawingView? = null
+    companion object
+    {
+        var path = Path()
+        var brushs = Paint()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_paint)
 
-        val parser: XmlPullParser = resources.getXml(R.layout.activity_paint)
-        val attributes: AttributeSet = Xml.asAttributeSet(parser)
 
-        var d = findViewById<DrawingView>(R.id.view)
-        var dv: DrawingView = DrawingView(this, attributes)
-        dv.edit("#0062ff", 10.3F)
+        var size = findViewById<SeekBar>(R.id.brushsize)
+        var x = 0f
+        size.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+            }
 
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                x = progress.toFloat()
+//                dv?.edit("#0062ff", x )
+            }
+        })
+        var reset = findViewById<ImageButton>(R.id.new_btn)
+        reset.setOnClickListener{
+//            dv?.reset()
+        }
+        var save = findViewById<ImageButton>(R.id.save_btn)
+        save.setOnClickListener {
+        }
+
+        dv = findViewById<DrawingView>(R.id.view);
+//        dv?.edit("#0062ff", x )
     }
 }
