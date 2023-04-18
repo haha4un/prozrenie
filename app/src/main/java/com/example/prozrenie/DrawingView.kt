41d73f96f3686e2b42ed1da0002123300ed73c14
haha4un <savelijstulev3872@gmail.com>
2023-04-18 20:04:59 +0500

    7.1

--- app/src/main/java/com/example/prozrenie/DrawingView.kt
+++ app/src/main/java/com/example/prozrenie/DrawingView.kt
@@ -3,88 +3,121 @@ package com.example.drawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import com.example.prozrenie.paint.Companion.brushs
-import com.example.prozrenie.paint.Companion.path
-import com.example.prozrenie.paint.Companion.s
 
+    class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
 
-class DrawingView(context: Context, attr: AttributeSet): View(context, attr) {
+        private var mDrawPath: CustomPath? = null
+        private var mCanvasBitmap: Bitmap? = null
+        private var mDrawPaint: Paint? = null
+        private var mCanvasPaint: Paint? = null
+        private var mBrushSize: Float = 0.toFloat()
+        private var color = Color.BLACK
+        private var canvas: Canvas? = null
+        private val mPaths = ArrayList<CustomPath>()
+        private val mUndoPaths = ArrayList<CustomPath>()
 
-    var params: ViewGroup.LayoutParams? =null
-    companion object
-    {
-        var pathList = ArrayList<Path>()
-        var colorList = ArrayList<Int>()
-        var sizeList = ArrayList<Float>()
-        var currentBrush = Color.GREEN
-    }
-    init{
-        brushs.isAntiAlias = true
-        brushs.color = currentBrush
-        brushs.style = Paint.Style.STROKE
-        brushs.strokeJoin = Paint.Join.ROUND
-        brushs.strokeWidth = s
-        params = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-    }
+        init {
+            setUpDrawing()
+        }
 
-    fun setStrokeWidth(strokeWidth: Float) {
-        brushs.strokeWidth = strokeWidth
-        postInvalidate()
-    }
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-        for (i in pathList.indices)
-        {
-            brushs.color = colorList[i]
-            brushs.strokeWidth = sizeList[i]
-            canvas.drawPath(pathList[i], brushs)
+        fun onClickUndo(){
+            if (mPaths.size > 0) {
+                mUndoPaths.add(mPaths.removeAt(mPaths.size-1))
+                invalidate()
+            }
+        }
+
+        private fun setUpDrawing() {
+            mDrawPaint = Paint()
+            mDrawPath = CustomPath(color, mBrushSize)
+            mDrawPaint!!.color = color
+            mDrawPaint!!.style = Paint.Style.STROKE
+            mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+            mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+            mCanvasPaint = Paint(Paint.DITHER_FLAG)
+            //mBrushSize = 20.toFloat()
+        }
+
+        override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+            super.onSizeChanged(w, h, oldw, oldh)
+            mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+            canvas = Canvas(mCanvasBitmap!!)
+        }
+
+        override fun onDraw(canvas: Canvas) {
+            super.onDraw(canvas)
+            canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+            for (path in mPaths) {
+                mDrawPaint!!.strokeWidth = path.brushThickness
+                mDrawPaint!!.color = path.color
+                canvas.drawPath(path, mDrawPaint!!)
+            }
+
+            if (!mDrawPath!!.isEmpty) {
+                mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+                mDrawPaint!!.color = mDrawPath!!.color
+                canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+            }
+
+        }
+
+        override fun onTouchEvent(event: MotionEvent?): Boolean {
+            val touchX = event?.x
+            val touchY = event?.y
+
+            when(event?.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    mDrawPath!!.color = color
+                    mDrawPath!!.brushThickness = mBrushSize
+
+                    mDrawPath!!.reset()
+                    if (touchX != null) {
+                        if (touchY != null) {
+                            mDrawPath!!.moveTo(touchX, touchY)
+                        }
+                    }
+                }
+
+                MotionEvent.ACTION_MOVE -> {
+                    if (touchX != null) {
+                        if (touchY != null) {
+                            mDrawPath!!.lineTo(touchX, touchY)
+                        }
+                    }
+                }
+
+                MotionEvent.ACTION_UP -> {
+                    mPaths.add(mDrawPath!!)
+                    mDrawPath = CustomPath(color, mBrushSize)
+                }
+
+                else -> return false
+            }
+
             invalidate()
+            return true
+        }
+
+        fun setSizeForBrush(newSize: Float) {
+            mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize, resources.displayMetrics)
+            mDrawPaint!!.strokeWidth = mBrushSize
+        }
+
+        fun setColor(newColor: String) {
+            color = Color.parseColor(newColor)
+            mDrawPaint!!.color = color
+        }
+        fun setColor(newColor: Int) {
+            color = newColor
+            mDrawPaint!!.color = color
+        }
+
+        internal inner class CustomPath(var color: Int, var brushThickness: Float): Path() {
+
         }
     }
-
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        val x = event?.x
-        val y = event?.y
-
-        when(event?.action){
-            MotionEvent.ACTION_DOWN -> {
-                path.moveTo(x!!, y!!)
-            }
-            MotionEvent.ACTION_MOVE -> {
-                path.lineTo(x!!, y!!)
-                pathList.add(path)
-                colorList.add(currentBrush)
-                sizeList.add(s)
-            }
-            else -> return false
-        }
-        postInvalidate()
-        return true
-    }
-    internal inner class CustomPath(var color: Int, var brushThickness: Float): Path(){
-    }
-    fun save(v: View): Bitmap {
-        v.isDrawingCacheEnabled = true
-        v.buildDrawingCache(true)
-        var b = Bitmap.createBitmap(v.drawingCache)
-        v.isDrawingCacheEnabled = false
-        return b
-    }
-//    fun save(): Bitmap? {
-//        val b = Bitmap.createBitmap(measuredWidth, measuredHeight, Bitmap.Config.ARGB_8888)
-//        val c = canvas
-//        layout(left, top, right, bottom)
-//        draw(c)
-//        return b
-//    }
-//    fun clearDrawing() {
-//        path?.reset() // Avoiding saving redo from Path().
-//        pathList.clear()
-//        colorList.clear()
-//        sizeList.clear()
-//        invalidate()
-//    }
-}
--- app/src/main/java/com/example/prozrenie/paint.kt
+++ app/src/main/java/com/example/prozrenie/paint.kt
@@ -1,130 +1,310 @@
 package com.example.prozrenie
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.Dialog
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.*
+import android.media.MediaScannerConnection
+import android.net.Uri
+import android.os.AsyncTask
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import android.provider.MediaStore
+import android.view.View
+import android.widget.FrameLayout
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.toColor
+import androidx.core.view.get
 import com.example.drawingapp.DrawingView
-import com.example.drawingapp.DrawingView.Companion.colorList
-import com.example.drawingapp.DrawingView.Companion.currentBrush
-import com.example.drawingapp.DrawingView.Companion.pathList
-import com.example.drawingapp.DrawingView.Companion.sizeList
+
 import yuku.ambilwarna.AmbilWarnaDialog
 import yuku.ambilwarna.AmbilWarnaDialog.OnAmbilWarnaListener
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
+import java.io.OutputStream
 import java.util.*
 
 
 class paint : AppCompatActivity() {
 
-    private var dv: DrawingView? = null
-    companion object
-    {
-        var path = Path()
-        var brushs = Paint()
-        var s = 10f
-    }
-    var defColor: Int = Color.BLACK
-    var bl: ImageButton ?= null
-    @SuppressLint("MissingInflatedId")
+    private var mImageButtonCurrentPaint: ImageButton? = null
+    var drawingView: DrawingView ?= null
+    var imageViewBackground: ImageView ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_paint)
         supportActionBar?.hide()
 
-        dv = findViewById<DrawingView>(R.id.view)
-//        dv?.setBackgroundResource(R.drawable.save)
+        drawingView = findViewById<DrawingView>(R.id.drawingView)
+        imageViewBackground = findViewById<ImageView>(R.id.imageViewBackground)
+        var paintColorsLayout = findViewById<LinearLayout>(R.id.paintColorsLayout)
+        var imageBrushSize = findViewById<SeekBar>(R.id.imageBrushButton)
+        var drawingFrameLayout = findViewById<FrameLayout>(R.id.drawingFrameLayout)
+        var imageSaveButton = findViewById<ImageButton>(R.id.imageSaveButton)
+        var imageUndoButton = findViewById<ImageButton>(R.id.imageUndoButton)
+        var imageGalleryButton = findViewById<ImageButton>(R.id.imageGalleryButton)
 
-        var size = findViewById<SeekBar>(R.id.brushsize)
+
+
+        mImageButtonCurrentPaint = paintColorsLayout[1] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.save))
+
         var x = 0f
-        size.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+        imageBrushSize.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
             override fun onStopTrackingTouch(seekBar: SeekBar) {
             }
-
             override fun onStartTrackingTouch(seekBar: SeekBar) {
             }
-
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
                 x = progress.toFloat()
-                s = x
-                dv?.setStrokeWidth(x)
-                path = Path()
+                drawingView?.setSizeForBrush(x)
             }
         })
-        var reset = findViewById<ImageButton>(R.id.new_btn)
-        reset.setOnClickListener{
-            pathList.clear()
-            colorList.clear()
-            sizeList.clear()
-            path.reset()
-            s= 10f
-            x= 10f
-            size.progress = 10
-            path = Path()
-        }
-        var save = findViewById<ImageButton>(R.id.save_btn)
-        save.setOnClickListener {
-           saveImage(dv?.save(dv!!)!!, "doesItWork")
-        }
 
-        bl = findViewById<ImageButton>(R.id.bl)
-        var gr = findViewById<ImageButton>(R.id.gr)
-        bl?.setOnClickListener{ openColorPicker()}
-        gr.setOnClickListener{currentColor(Color.GREEN)}
+        imageGalleryButton.setOnClickListener {
+            if(isReadStorageAllowed()){
+                val pickPhotoIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(pickPhotoIntent, GALLERY)
 
-        var add = findViewById<ImageButton>(R.id.addimg_btn)
-        var i =  0
-        add.setOnClickListener{
-            i++
-            when (i) {
-                1 -> dv?.setBackgroundResource(R.drawable.i1)
-                2 -> dv?.setBackgroundResource(R.drawable.i2)
-                3 -> dv?.setBackgroundResource(R.drawable.i3)
-                4 -> dv?.setBackgroundResource(R.drawable.i4)
-                5 -> dv?.setBackgroundColor(Color.WHITE)
-                6 -> i = 0
+            }else{
+                requestStoragePermission()
             }
+        }
 
+        imageUndoButton.setOnClickListener {
+            drawingView?.onClickUndo()
+        }
+
+        imageSaveButton.setOnClickListener {
+            if(isReadStorageAllowed()) {
+                BitmapAsyncTask(getBitmapFromView(drawingFrameLayout), this).execute()
+            }else {
+                requestStoragePermission()
+            }
         }
     }
-    fun openColorPicker() {
-        val colorPicker = AmbilWarnaDialog(this, defColor, object : OnAmbilWarnaListener {
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == GALLERY){
+                try {
+                    if(data!!.data != null){
+                        imageViewBackground?.visibility = View.VISIBLE
+                        imageViewBackground?.setImageURI(data.data)
+                    }else{
+                        Toast.makeText(this, "error in parsing the image", Toast.LENGTH_SHORT).show()
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+//    private fun showBrushSizeChooseDialog(){
+//        val brushDialog = Dialog(this)
+//        brushDialog.setContentView(R.layout.dialog_brush_size)
+//        brushDialog.setTitle("Brush size: ")
+//        val smallButton = brushDialog.smallBrushButton
+//        smallButton.setOnClickListener {
+//            drawingView.setSizeForBrush(10.toFloat())
+//            brushDialog.dismiss()
+//        }
+//        val mediumButton = brushDialog.mediumBrushButton
+//        mediumButton.setOnClickListener {
+//            drawingView.setSizeForBrush(20.toFloat())
+//            brushDialog.dismiss()
+//        }
+//        val largeButton = brushDialog.largeBrushButton
+//        largeButton.setOnClickListener {
+//            drawingView.setSizeForBrush(30.toFloat())
+//            brushDialog.dismiss()
+//        }
+//
+//        brushDialog.show()
+//    }
+
+    fun paintClicked(view: View) {
+        if (view !== mImageButtonCurrentPaint) {
+            val imageButton = view as ImageButton
+            val colorTag = imageButton.tag.toString()
+
+            if (colorTag == "#ffffcc99")
+                openColorPicker(imageButton)
+            else
+                drawingView?.setColor(colorTag)
+
+            imageButton.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.pallet_pressed))
+
+            mImageButtonCurrentPaint!!.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.pallet_normal))
+
+            mImageButtonCurrentPaint = view
+        }
+    }
+    fun openColorPicker(i: ImageButton) {
+        val colorPicker = AmbilWarnaDialog(this, R.color.skin, object : OnAmbilWarnaListener {
             override fun onCancel(dialog: AmbilWarnaDialog) {}
+            @RequiresApi(Build.VERSION_CODES.O)
             override fun onOk(dialog: AmbilWarnaDialog, color: Int) {
-                defColor = color
-                bl?.setBackgroundColor(defColor)
-                currentColor(defColor)
+                i.setBackgroundColor(color)
+                drawingView?.setColor(color)
             }
         })
         colorPicker.show()
     }
-    private fun currentColor(color: Int)
-    {
-        currentBrush = color
-        path = Path()
-    }
-    fun saveImage(finalBitmap: Bitmap, image_name: String) {
-        val root = Environment.getExternalStorageDirectory().toString()
-        val myDir = File("$root/Pictures")
-        myDir.mkdirs()
-        val fname = "Image-$image_name.jpg"
-        val file = File(myDir, fname)
-        if (file.exists()) file.delete()
-        Toast.makeText(this, "load in $myDir", Toast.LENGTH_SHORT).show()
-        try {
-            val out = FileOutputStream(file)
-            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out)
-            out.flush()
-            out.close()
-        } catch (e: java.lang.Exception) {
-            e.printStackTrace()
-            Toast.makeText(this, "$e", Toast.LENGTH_SHORT).show()
+
+    private fun requestStoragePermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE).toString())){
+            Toast.makeText(this, "Need permission to add background", Toast.LENGTH_SHORT).show()
         }
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE), STORAGE_PERMISSION_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == STORAGE_PERMISSION_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Now can read storage", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "oops cant read storage", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun isReadStorageAllowed(): Boolean{
+        val result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun getBitmapFromView(view: View): Bitmap {
+        val returnBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(returnBitmap)
+        val bgDrawable = view.background
+        if(bgDrawable != null){
+            bgDrawable.draw(canvas)
+        }else{
+            canvas.drawColor(Color.WHITE)
+        }
+
+        view.draw(canvas)
+
+        return returnBitmap
+    }
+
+    private inner class BitmapAsyncTask(val mBitmap: Bitmap, val context: Context): AsyncTask<Any, Void, String>(){
+
+        private lateinit var  mProgressDialog: Dialog
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+//            showProgressDialog()
+        }
+
+        override fun doInBackground(vararg params: Any?): String {
+            var result = ""
+
+            try {
+                val bytes = ByteArrayOutputStream()
+                val fileName = "DrawingApp_" + System.currentTimeMillis()/1000 + ".png"
+                var fos : OutputStream? = null
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    context.contentResolver?.also { resolver ->
+                        val contentValues = ContentValues().apply {
+                            put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+                            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                        }
+                        val imageUri: Uri? =
+                            resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+                        fos = imageUri?.let { resolver.openOutputStream(it) }
+                    }
+                } else {
+                    val imagesDir =
+                        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                    val image = File(imagesDir, fileName)
+                    fos = FileOutputStream(image)
+                }
+                fos?.use {
+                    mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+                }
+                fos?.write(bytes.toByteArray())
+                fos?.close()
+                result = fileName
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            return result
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            cancelProgressDialog()
+            if (result!!.isNotEmpty()) {
+                Toast.makeText(
+                    this@paint,
+                    "File saved successfully :$result",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    this@paint,
+                    "Something went wrong while saving the file.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            MediaScannerConnection.scanFile(this@paint, arrayOf(result), null){
+                    path, uri -> val shareIntent = Intent()
+                shareIntent.action = Intent.ACTION_SEND
+                shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+                shareIntent.type = "image/png"
+
+                startActivity(Intent.createChooser(shareIntent, "Share"))
+            }
+        }
+
+//        private fun showProgressDialog(){
+//            mProgressDialog = Dialog(this@paint)
+//            mProgressDialog.setContentView(R.layout.dialog_custom_progress)
+//            mProgressDialog.show()
+//        }
+
+        private fun cancelProgressDialog(){
+            mProgressDialog.dismiss()
+        }
+
+    }
+
+    companion object{
+        private const val STORAGE_PERMISSION_CODE = 1
+        private const val GALLERY = 2
     }
 }
