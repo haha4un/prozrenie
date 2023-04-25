f8e68f9123beec23e3817ca9544b2af6493705c3
haha4un <savelijstulev3872@gmail.com>
2023-04-25 19:16:10 +0500

    8.2.2

--- app/src/main/java/com/example/prozrenie/MainActivity.kt
+++ app/src/main/java/com/example/prozrenie/MainActivity.kt
@@ -2,17 +2,30 @@ package com.example.prozrenie
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+
+    private var x1 = 0f
+    private var x2 = 0f
+    val MIN_DISTANCE = 150
+
+    var limitOfGames = 3
+    var i = 0
+    @SuppressLint("MissingInflatedId", "ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         getSupportActionBar()?.hide();
+
 
         var create_card = findViewById<Button>(R.id.create_card)
         create_card.setOnClickListener{
@@ -26,20 +39,89 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        var game_pict = findViewById<ImageButton>(R.id.game_pict)
-        game_pict.setOnClickListener {
-            var intent = Intent(this, imges::class.java)
-            startActivity(intent)
-        }
-        var game_paint = findViewById<ImageButton>(R.id.game_paint)
-        game_paint.setOnClickListener {
-            var intent = Intent(this, paint::class.java)
-            startActivity(intent)
-        }
-        var game_relax = findViewById<ImageButton>(R.id.game_relax)
-        game_relax.setOnClickListener {
-            var intent = Intent(this, relax_game::class.java)
-            startActivity(intent)
-        }
+        var games = findViewById<ImageButton>(R.id.games)
+        games.setOnTouchListener(
+            object : View.OnTouchListener {
+                override fun onTouch(v: View?, event: MotionEvent): Boolean {
+                    when (event.action) {
+                        MotionEvent.ACTION_DOWN -> x1 = event.x
+                        MotionEvent.ACTION_UP -> {
+                            x2 = event.x
+                            val deltaX = x2 - x1
+                            if (deltaX < 0) {
+                                Toast.makeText(
+                                    this@MainActivity,
+                                    "Right to Left swipe",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                i++
+                            } else if (deltaX > 0) {
+                                Toast.makeText(
+                                    this@MainActivity,
+                                    "Left to Right swipe",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                i--
+                            }
+                        }
+                    }
+                    toNextActivity(i, games)
+                    return false
+                }
+            })
     }
+    fun toNextActivity(i:Int, view: ImageView) : Int
+    {
+        var j = i
+        if (j > limitOfGames){
+            j = 1
+            this.i = 1
+        }
+        if (j <= 0) {
+            j = limitOfGames
+            this.i = limitOfGames
+        }
+        when(j)
+        {
+            1 -> {
+                view.setImageResource(R.drawable.gi1)
+                return 1
+            }
+            2 -> {
+                view.setImageResource(R.drawable.gi2)
+                return 2
+            }
+            3 -> {
+                view.setImageResource(R.drawable.gi3)
+                return 2
+            }
+        }
+        return 0
+    }
+//    view.setOnTouchListener(
+//    object : View.OnTouchListener {
+//        override fun onTouch(v: View?, event: MotionEvent): Boolean {
+//            when (event.action) {
+//                MotionEvent.ACTION_DOWN -> x1 = event.x
+//                MotionEvent.ACTION_UP -> {
+//                    x2 = event.x
+//                    val deltaX = x2 - x1
+//                    if (deltaX < 0) {
+//                        Toast.makeText(
+//                            this@MainActivity,
+//                            "Right to Left swipe",
+//                            Toast.LENGTH_SHORT
+//                        ).show()
+//                    } else if (deltaX > 0) {
+//                        Toast.makeText(
+//                            this@MainActivity,
+//                            "Left to Right swipe",
+//                            Toast.LENGTH_SHORT
+//                        ).show()
+//                    }
+//                }
+//            }
+//            return false
+//        }
+//    })
 }
--- app/src/main/java/com/example/prozrenie/relax_game.kt
+++ app/src/main/java/com/example/prozrenie/relax_game.kt
@@ -8,11 +8,17 @@ import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
 import android.widget.MediaController
+import android.widget.Toast
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class relax_game : AppCompatActivity() {
+
+    private var x1 = 0f
+    private var x2 = 0f
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_relax_game)
@@ -21,27 +27,56 @@ class relax_game : AppCompatActivity() {
         var i = 0
         var x = "android.resource://$packageName/"
 
-        var ch = findViewById<Button>(R.id.choose_vid)
+//        var ch = findViewById<Button>(R.id.choose_vid)
         var v = findViewById<VideoView>(R.id.videoView)
 
-        ch.setOnClickListener {
-            i++
-            v.setVisibility(VideoView.VISIBLE)
-            var path = x
-            when (i) {
-                 1 -> path += R.raw.v1
-                 2-> path += R.raw.v2
-            }
-            var uri: Uri = Uri.parse(path)
-            v.setVideoURI(uri)
-            val m = MediaController(this)
-            v.setMediaController(m)
-            m.setAnchorView(v)
-            v.start()
-        }
-
+//        ch.setOnClickListener {
+//            i++
+//            v.setVisibility(VideoView.VISIBLE)
+//            var path = x
+//            when (i) {
+//                 1 -> path += R.raw.v1
+//                 2-> path += R.raw.v2
+//            }
+//            var uri: Uri = Uri.parse(path)
+//            v.setVideoURI(uri)
+//            val m = MediaController(this)
+//            v.setMediaController(m)
+//            m.setAnchorView(v)
+//            v.start()
+//        }
+        v.setOnTouchListener(
+            object : View.OnTouchListener {
+                override fun onTouch(v: View?, event: MotionEvent): Boolean {
+                    when (event.action) {
+                        MotionEvent.ACTION_DOWN -> x1 = event.x
+                        MotionEvent.ACTION_UP -> {
+                            x2 = event.x
+                            val deltaX = x2 - x1
+                            if (deltaX < 0) {
+                                Toast.makeText(
+                                    this@relax_game,
+                                    "Right to Left swipe",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            } else if (deltaX > 0) {
+                                Toast.makeText(
+                                    this@relax_game,
+                                    "Left to Right swipe",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                    }
+                    return false
+                }
+            })
 //        videoView.setOnCompletionListener(OnCompletionListener {
 //            // Video Playing is completed
 //        })
+    }
+    fun create ():String
+    {
+        return ""
     }
 }
