03fd99d3e03f3ccc1f294d31dec736142ed7bfbb
haha4un <savelijstulev3872@gmail.com>
2023-05-20 22:34:31 +0500

    v10.7 - cleaned imges_game_1 and imges_game_2's code

--- app/src/main/java/com/example/prozrenie/imges_game_1.kt
+++ app/src/main/java/com/example/prozrenie/imges_game_1.kt
@@ -15,16 +15,9 @@ class imges_game_1 : AppCompatActivity() {
 
     }
     fun Clicked(view: View) {
-         var x = view.tag.toString().toInt()
-            when (x) {
-                1 -> intent.putExtra("IMG", "g1")
-                2 -> intent.putExtra("IMG", "g2")
-                3 -> intent.putExtra("IMG", "g3")
-                4 -> intent.putExtra("IMG", "g4")
-                5 -> intent.putExtra("IMG", "g5")
-                6 -> intent.putExtra("IMG", "g6")
-                7 -> intent.putExtra("IMG", "g7")
-            }
-            startActivity(intent)
+         var x = view.tag.toString()
+         var i: Intent = Intent(this, imges_game_2::class.java)
+            i.putExtra("IMG", "g$x");
+            startActivity(i)
         }
     }
