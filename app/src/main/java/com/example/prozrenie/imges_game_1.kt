7a4beb97295e84b7d469d7f73fb7f44823cc118e
haha4un <savelijstulev3872@gmail.com>
2023-05-20 22:19:44 +0500

    v10.7

--- app/src/main/java/com/example/prozrenie/imges_game_1.kt
+++ app/src/main/java/com/example/prozrenie/imges_game_1.kt
@@ -14,11 +14,17 @@ class imges_game_1 : AppCompatActivity() {
 
 
     }
-    fun paintClicked(view: View) {
+    fun Clicked(view: View) {
          var x = view.tag.toString().toInt()
             when (x) {
-                1 ->
+                1 -> intent.putExtra("IMG", "g1")
+                2 -> intent.putExtra("IMG", "g2")
+                3 -> intent.putExtra("IMG", "g3")
+                4 -> intent.putExtra("IMG", "g4")
+                5 -> intent.putExtra("IMG", "g5")
+                6 -> intent.putExtra("IMG", "g6")
+                7 -> intent.putExtra("IMG", "g7")
             }
-
+            startActivity(intent)
         }
     }
