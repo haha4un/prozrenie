a07393f62fb59d09ec8c319ea59d80d4ed7c8003
haha4un <savelijstulev3872@gmail.com>
2023-05-21 11:43:15 +0500

    v10.7.1 - ahhh, here we go again!

--- app/src/main/java/com/example/prozrenie/relax_game.kt
+++ app/src/main/java/com/example/prozrenie/relax_game.kt
@@ -1,95 +1,98 @@
 package com.example.prozrenie
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.MediaController
+import android.widget.Toast
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStream
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
 
 
 class relax_game : AppCompatActivity() {
 
+    private var x1 = 0f
+    private var x2 = 0f
+
+    var limitOfVideos = 3
+    var i = 0
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_relax_game)
         supportActionBar?.hide()
 
+        var x = "android.resource://$packageName/"
+
+        var ch = findViewById<Button>(R.id.choose_vid)
         var v = findViewById<VideoView>(R.id.videoView)
 
-        val vsrc =
-            "https://drive.google.com/file/d/1NiLtsdwlCdPMZg6B8sue7OCj8RcCDj0_/view?usp=sharing"
-
-        v.setVideoPath(downloadUrl(vsrc));
-        v.setMediaController(MediaController(this));
-        v.requestFocus(0);
-        v.start();
+        var temp_for_name = ""
+        var stopper = false
+        ch.setOnTouchListener(
+            object : View.OnTouchListener {
+                override fun onTouch(view: View?, event: MotionEvent): Boolean {
+                    when (event.action) {
+                        MotionEvent.ACTION_DOWN -> x1 = event.x
+                        MotionEvent.ACTION_UP -> {
+                            x2 = event.x
+                            val deltaX = x2 - x1
+                            if (deltaX < 0) {
+                                i++
+                                stopper = false
+                            } else if (deltaX > 0) {
+                                i--
+                                stopper = false
+                            }
+                            else if (deltaX == 0f)
+                            {
+                                if (!stopper){
+                                    Toast.makeText(this@relax_game, "Vid: $temp_for_name", Toast.LENGTH_SHORT).show()
+                                    v.pause()
+                                    stopper = true
+                                }
+                                else if (stopper)
+                                    stopper = false
+                            }
+                        }
+                    }
+                    if (!stopper){
+                        temp_for_name = toNextVideo(i, v, x)
+                    }
+                    return false
+                }
+            })
     }
+    fun toNextVideo(i:Int, view: VideoView, txt: String)  : String
+    {
+        var path = txt
+        var j = i
 
-    @Throws(IOException::class)
-    fun downloadUrl(myurl: String?): String? {
-        var `is`: InputStream? = null
-        return try {
-            val url = URL(myurl)
-            val conn =
-                url.openConnection() as HttpURLConnection
-            conn.readTimeout = 10000
-            conn.connectTimeout = 15000
-            conn.requestMethod = "GET"
-            conn.doInput = true
-            conn.connect()
-            `is` = conn.inputStream
-            readIt(`is`)
-        } finally {
-            `is`?.close()
+        if (j > limitOfVideos){
+            j = 1
+            this.i = 1
         }
-    }
-
-    @Throws(IOException::class)
-    fun readIt(stream: InputStream?): String? {
-        val reader = BufferedReader(InputStreamReader(stream, "UTF-8"))
-        val sb = StringBuilder()
-        var line: String
-        while (reader.readLine().also { line = it } != null) {
-            if (line.contains("fmt_stream_map")) {
-                sb.append(
-                    """
-                    $line
-                    
-                    """.trimIndent()
-                )
-                break
-            }
+        if (j <= 0) {
+            j = limitOfVideos
+            this.i = limitOfVideos
         }
-        reader.close()
-        val result: String = decode(sb.toString())!!
-        val url = result.split("\\|".toRegex()).dropLastWhile { it.isEmpty() }
-            .toTypedArray()
-        return url[1]
-    }
-    fun decode(`in`: String): String? {
-        var working = `in`
-        var index: Int
-        index = working.indexOf("\\u")
-        while (index > -1) {
-            val length = working.length
-            if (index > (length - 6)) break
-            val numStart = index + 2
-            val numFinish = numStart + 4
-            val substring = working.substring(numStart, numFinish)
-            val number = substring.toInt(16)
-            val stringStart = working.substring(0, index)
-            val stringEnd = working.substring(numFinish)
-            working = stringStart + number.toChar() + stringEnd
-            index = working.indexOf("\\u")
+        when (j) {
+            1 -> path += com.example.prozrenie.R.raw.v1
+            2-> path += com.example.prozrenie.R.raw.v2;
+            3-> path += com.example.prozrenie.R.raw.v2;
         }
-        return working
+        var uri: Uri = Uri.parse(path)
+        view.setVideoURI(uri)
+        val m = MediaController(this)
+        view.setMediaController(m)
+        m.setAnchorView(view)
+        view.start()
+        return uri.toString()
     }
 }
