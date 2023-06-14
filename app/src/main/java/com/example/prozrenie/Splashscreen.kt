939be28640928a84c0ed655b65c775b7114cdefe
haha4un <savelijstulev3872@gmail.com>
2023-06-14 11:29:41 +0500

    v10.8.4.2 - some design features

--- app/src/main/java/com/example/prozrenie/Splashscreen.kt
+++ app/src/main/java/com/example/prozrenie/Splashscreen.kt
@@ -21,34 +21,29 @@ class Splashscreen : AppCompatActivity() {
         if (!isOnline(this))
         {
             Toast.makeText(this, "Нет соеденения с сетью!", Toast.LENGTH_SHORT).show()
-//            Handler().postDelayed({
-//                val intent = Intent(this, Splashscreen::class.java)
-//                startActivity(intent)
-//                finish()
-//            }, 5000)
         }
 
-//        var dialog = Dialog(this)
-//        dialog.setContentView(R.layout.checker)
-//        var pass = dialog.findViewById<EditText>(R.id.pass_checker)
-//        var login = dialog.findViewById<EditText>(R.id.login_checker)
-//        var ok = dialog.findViewById<Button>(R.id.check_checker)
-//        ok.setOnClickListener {
-//            if (login.text.toString() == "h4n/mur/key" && pass.text.toString() == "h4nkm" || pass.text.toString() == "g")
-//            {
-//                val intent = Intent(this, MainActivity ::class.java)
-//                startActivity(intent)
-//                finish()
-//            }
-//            else
-//            {
-//                Toast.makeText(this, "Не правильная связка пароль-логин!", Toast.LENGTH_SHORT).show()
-//                val intent = Intent(this,  Splashscreen::class.java)
-//                startActivity(intent)
-//                finish()
-//            }
-//        }
-//        dialog.show()
+        var dialog = Dialog(this)
+        dialog.setContentView(R.layout.checker)
+        var pass = dialog.findViewById<EditText>(R.id.pass_checker)
+        var login = dialog.findViewById<EditText>(R.id.login_checker)
+        var ok = dialog.findViewById<Button>(R.id.check_checker)
+        ok.setOnClickListener {
+            if (login.text.toString() == "h4n/mur/key" && pass.text.toString() == "h4nkm" || pass.text.toString() == "g")
+            {
+                val intent = Intent(this, MainActivity ::class.java)
+                startActivity(intent)
+                finish()
+            }
+            else
+            {
+                Toast.makeText(this, "Неправильная связка пароль-логин!", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this,  Splashscreen::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+        dialog.show()
     }
     fun isOnline(context: Context): Boolean {
         val cm = context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
