13cc9b829c352255355a8cab856ac39797c8de47
haha4un <savelijstulev3872@gmail.com>
2023-07-31 17:59:34 +0500

    v10.10.4.2 - pre-set for 10.11

--- app/src/main/java/com/example/prozrenie/cross_game.kt
+++ app/src/main/java/com/example/prozrenie/cross_game.kt
@@ -5,12 +5,15 @@ import android.graphics.drawable.GradientDrawable.Orientation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.view.get
+import androidx.core.view.updateLayoutParams
 import kotlin.random.Random
 
 class cross_game : AppCompatActivity() {
@@ -18,6 +21,7 @@ class cross_game : AppCompatActivity() {
     var cl: LinearLayout?= null
     var sc: TextView ?= null
     var sci: Int = 0
+    var RAN_POS = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,13 +31,16 @@ class cross_game : AppCompatActivity() {
         cl = findViewById(R.id.common_lay)
         sc = findViewById(R.id.scores_cross)
 
-
         makeTheLines()
     }
 
     fun makeTheLines()
     {
-      for (i in 1..8) {
+      if (cl?.childCount != 0) {
+          cl?.removeAllViews()
+      }
+
+      for (i in 0..8) {
           var nl = LinearLayout(this)
           nl.setOrientation(LinearLayout.HORIZONTAL)
           cl?.addView(nl)
@@ -43,20 +50,44 @@ class cross_game : AppCompatActivity() {
 
     fun colorLines()
     {
+        RAN_POS = Random.nextInt(2,48)
         var cont = 0
         for (i in 0 until cl!!.childCount)
         {
             val j = cl!!.getChildAt(i) as LinearLayout
             cont++
-            for (ij in 1..8)
+            for (ij in 0..8)
             {
                 var ni = ImageView(this)
-                if (cont % 2 == 0)
-                    ni.setImageResource(R.drawable.hexagon_4)
-                else
-                    ni.setImageResource(R.drawable.hexagon_3)
-                ni.maxWidth = 100
-                ni.maxHeight = 100
+                ni.maxWidth = 25
+                ni.maxHeight = 25
+                if (cont % 2 == 0 && cont!= RAN_POS) {
+                    ni.setImageResource(R.drawable.round_2)
+                    ni.setOnClickListener {
+                        sci--
+                        sc?.text = sci.toString()
+                        makeTheLines()
+                    }
+                }
+                else if (cont == RAN_POS)
+                {
+                    ni.setImageResource(R.drawable.round_3)
+                    ni.setOnClickListener {
+                        sci++
+                        Toast.makeText(this, "Congrats!", Toast.LENGTH_SHORT).show()
+                        sc?.text = sci.toString()
+                        cont = 0
+                        makeTheLines()
+                    }
+                }
+                else {
+                    ni.setImageResource(R.drawable.round_1)
+                    ni.setOnClickListener {
+                        sci--
+                        sc?.text = sci.toString()
+                        makeTheLines()
+                    }
+                }
                 j.addView(ni)
                 cont++
             }
