e370f4f059b4070060f961f5bda52618e8f12bbf
haha4un <savelijstulev3872@gmail.com>
2023-09-12 17:50:15 +0500

    v10.11.5.2

--- app/src/main/java/com/example/prozrenie/cross_game.kt
+++ app/src/main/java/com/example/prozrenie/cross_game.kt
@@ -32,7 +32,7 @@ class cross_game : AppCompatActivity() {
     var LENGHT = 8
     var SIZES = 200
     var SPEED = 1000
-    var MULHL = HEIGHT*LENGHT
+    var MULHL = HEIGHT * LENGHT
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,47 +42,32 @@ class cross_game : AppCompatActivity() {
         cl = findViewById(R.id.common_lay)
         sc = findViewById(R.id.scores_cross)
 
-        val w = findViewById<EditText>(R.id.height_cross)
-        w.setText(LENGHT.toString())
-        val h = findViewById<EditText>(R.id.lenght_cross)
-        h.setText(HEIGHT.toString())
-        val s = findViewById<EditText>(R.id.sizes_cross)
-        s.setText(SIZES.toString())
-        val sp = findViewById<EditText>(R.id.speed_cross)
-        sp.setText(SPEED.toString())
-        val sv = findViewById<Button>(R.id.submit_data)
-        sv.setOnClickListener {
-            HEIGHT = h.text.toString().toInt()
-            LENGHT = w.text.toString().toInt()
-            SIZES = s.text.toString().toInt()
-            SPEED = sp.text.toString().toInt()
-            MULHL =HEIGHT* LENGHT
-            RAN_POS = Random.nextInt(0, MULHL)
-        }
-        RAN_POS = Random.nextInt(0, MULHL)
+        RAN_POS = randPos()
         starter()
     }
 
     fun starter() = GlobalScope.launch(Dispatchers.Main) {
-        repeat(99999999){
+        repeat(99999999) {
             delay(SPEED.toLong())
-            main(1,2)
+            main(1, 2)
             delay(SPEED.toLong())
-            main(2,1)
+            main(2, 1)
             delay(SPEED.toLong())
-            main(1,2)
+            main(1, 2)
             delay(SPEED.toLong())
-            main(2,1)
+            main(2, 1)
         }
     }
 
     fun main(f: Byte, s: Byte) = runBlocking {
         makeTheLines()
-        colorLines(f,s)
+        if ((f == 1.toString().toByte()) and (s == 2.toString().toByte()))
+            colorLines(f, s, 1)
+        else if ((f == 2.toString().toByte()) and (s == 1.toString().toByte()))
+            colorLines(f, s, 2)
     }
 
-    suspend fun makeTheLines()
-    {
+    suspend fun makeTheLines() {
         if (cl?.childCount != 0) {
             cl?.removeAllViews()
         }
@@ -94,48 +79,57 @@ class cross_game : AppCompatActivity() {
         }
     }
 
-    suspend fun colorLines(f: Byte, s: Byte)
-    {
+    fun randPos(): Int {
+        while (true) {
+            var t = Random.nextInt(2, MULHL - 2)
+            if ((t % 2 != 0)) {
+                return t
+            }
+        }
+        return 0
+    }
+
+    suspend fun colorLines(f: Byte, s: Byte, turng: Byte) {
         var fs = "$PRE_FIX$f"
         var ss = "$PRE_FIX$s"
+        var fd = "$PRE_FIX$3"
         var cont = 0
         var count_el = 0
-        for (i in 0 until  cl!!.childCount-1)
-        {
+        for (i in 0 until cl!!.childCount - 1) {
             val j = cl!!.getChildAt(i) as LinearLayout
             cont++
-            for (ij in 0 until LENGHT)
-            {
+            for (ij in 0 until LENGHT) {
                 var ni = ImageView(this)
                 ni.setLayoutParams(ViewGroup.LayoutParams(SIZES, SIZES))
 
-                if (cont % 2 == 0 && count_el!= RAN_POS) {
-                    ni.setImageResource(resources.getIdentifier(fs, "drawable", packageName))
-                    ni.setOnClickListener {
-                        sci--
-                        sc?.text = sci.toString()
-                        main(s,f)
+                // The ides is good, isnt it?)
+                if ((count_el == RAN_POS) or (count_el == RAN_POS - LENGHT) or (count_el == RAN_POS + LENGHT) or (count_el == RAN_POS + 1) or (count_el == RAN_POS - 1)) {
+                    when (turng) {
+                        1.toString().toByte() -> ni.setImageResource(R.drawable.round_1);
+                        2.toString().toByte() -> ni.setImageResource(R.drawable.round_2);
                     }
-                }
-                else if (count_el == RAN_POS)
-                {
-                    ni.setImageResource(R.drawable.round_3)
                     ni.setOnClickListener {
                         sci++
                         Toast.makeText(this, "Congrats!", Toast.LENGTH_SHORT).show()
                         sc?.text = sci.toString()
                         cont = 0
-                        RAN_POS = Random.nextInt(0, MULHL)
-                        main(s,f)
+                        RAN_POS = randPos()
+                        main(f, s)
                         return@setOnClickListener
                     }
-                }
-                else if (cont % 2 != 0 && count_el!= RAN_POS) {
+                } else if (cont % 2 == 0 && count_el != RAN_POS) {
+                    ni.setImageResource(resources.getIdentifier(fs, "drawable", packageName))
+                    ni.setOnClickListener {
+                        sci--
+                        sc?.text = sci.toString()
+                        main(s, f)
+                    }
+                } else if (cont % 2 != 0 && count_el != RAN_POS) {
                     ni.setImageResource(resources.getIdentifier(ss, "drawable", packageName))
                     ni.setOnClickListener {
                         sci--
                         sc?.text = sci.toString()
-                        main(s,f)
+                        main(s, f)
                     }
                 }
                 count_el++
@@ -144,6 +138,7 @@ class cross_game : AppCompatActivity() {
             }
         }
     }
+
     fun question_btn_cross(view: View) {
         var dialog = Dialog(this)
         dialog.setContentView(R.layout.questtext)
@@ -151,6 +146,34 @@ class cross_game : AppCompatActivity() {
         txt.setText(R.string.howto_cross)
         var ok = dialog.findViewById<Button>(R.id.offthedial)
         ok.setOnClickListener { dialog.dismiss() }
+        dialog.show()
+    }
+
+    fun resize_btn_click(view: View){
+        var dialog = Dialog(this)
+        dialog.setContentView(R.layout.resizement)
+        var height = dialog.findViewById<TextView>(R.id.height_cross)
+        height.setText(HEIGHT.toString())
+        var length = dialog.findViewById<TextView>(R.id.lenght_cross)
+        length.setText(LENGHT.toString())
+        var speed = dialog.findViewById<TextView>(R.id.speed_cross)
+        speed.setText(SPEED.toString())
+        var size = dialog.findViewById<TextView>(R.id.sizes_cross)
+        size.setText(SIZES.toString())
+        var ok = dialog.findViewById<Button>(R.id.submit_data)
+        ok.setOnClickListener {
+            if (HEIGHT <= 0 || LENGHT<=0||SIZES<=0||SPEED<=0){
+                Toast.makeText(this, "Данные должны быть не нулевыми!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            HEIGHT = height.text.toString().toInt()
+            LENGHT = length.text.toString().toInt()
+            SIZES = size.text.toString().toInt()
+            SPEED = speed.text.toString().toInt()
+            MULHL =HEIGHT* LENGHT
+            RAN_POS = randPos()
+            dialog.dismiss()
+        }
         dialog.show()
     }
 }
